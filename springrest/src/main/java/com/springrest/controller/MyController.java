8c094b7fcc0304165e3a63a7d85de69083f3fa17
ravindercharan11 <149073426+ravindercharan11@users.noreply.github.com>
2023-11-02 15:55:42 +0530

    Update MyController.java
    add comment for better understanding

--- springrest/src/main/java/com/springrest/controller/MyController.java
+++ springrest/src/main/java/com/springrest/controller/MyController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
+    //create get api using springboot//
     @GetMapping("/house")
     public String home(){
         return "this is my get api";
